deb33e5c38026d2e95ca615bbcb95dfd25772e63
Camily Milsoni <camily_milsoni@hotmail.com>
2023-06-06 19:00:36 -0300

    Classe Cliente

---
+++ cCliente.h
@@ -0,0 +1,35 @@
+#ifndef CCLIENTE_H
+#define CCLIENTE_H
+#include <iostream>
+
+
+using namespace std;
+
+class cCliente {
+public:
+    cCliente();
+    cCliente(const cCliente& orig);
+    virtual ~cCliente();
+      
+    struct Cliente{
+        string nome;
+        string endereco;
+        unsigned long int codigo;
+        struct Cliente* prox;
+    };
+    
+    Cliente* primeiro = NULL;
+    
+    Cliente* inserir_cliente(Cliente* primeiro);
+    void listar_cliente(Cliente* primeiro);
+    Cliente* excluir_cliente(Cliente* primeiro);
+    void alterar_cliente(Cliente* primeiro);
+    void buscar_cliente(Cliente* primeiro);
+    void menuCliente();
+      
+private:
+
+};
+
+#endif /* CCLIENTE_H */
+
---
+++ main.cpp
@@ -0,0 +1,65 @@
+#include <cstdlib>
+#include <iostream>
+#include "cCliente.h"
+#include "cProduto.h"
+#include "cVenda.h"
+
+using namespace std;
+
+void menuPrincipal();
+
+int main(int argc, char** argv) {
+    menuPrincipal();
+    return 0;
+}
+
+void menuPrincipal() {
+    cCliente* objCliente = new cCliente;
+    
+    int opcao = -1;
+   
+    while(opcao != 0){
+        system("clear||cls");
+        cout << "" << endl;
+        cout << "    SISTEMA DE VENDAS     " << endl;
+        cout << "" << endl;
+        cout << "    1. Cliente" << endl;
+        cout << "------------------------" << endl;
+        cout << "    2. Produto" << endl;
+        cout << "------------------------" << endl;
+        cout << "    3. Venda" << endl;
+        cout << "------------------------" << endl;
+        cout << "    0. Sair" << endl;
+        cout << "" << endl;
+        cout << "INFORME A SUA OPÇÃO: ";
+        cin >> opcao;
+        
+        switch (opcao){
+        case 1:
+            system("clear||cls");
+            objCliente->menuCliente();
+            break;
+
+        case 2:
+            system("clear||cls");
+
+            break;
+
+        case 3:
+            system("clear||cls");
+
+            break;
+
+        case 0:
+            system("clear||cls");
+            opcao = 0;
+            break;
+            
+        default:
+            cout << "Opção inválida." << endl;
+            cout << endl << "**PRESSIONE QUALQUER TECLA PARA VOLTAR AO MENU PRINCIPAL**";
+            cin.get();
+            cin.get();
+        }
+    }
+}
